006b57b9afcfdf5eb487cddb01746a6e0b8b08d7
John L. Godlee <johngodlee@gmail.com>
2021-04-23 11:21:25 +0100

    Added info on spatial mingling

--- workflow/build.sh
+++ workflow/build.sh
@@ -7,6 +7,7 @@ cp ../img/grass_3d.png img
 cp ../img/crown.pdf img
 cp ../img/noise_vis.png img
 cp ../img/tls_hemi_compare.pdf img
+cp ../img/map.png img
 
 # Adjust some snippets
 sed -i '7s/.*/{Site} \& \\multicolumn{1}{p{2.5cm}}{\\centering MAT \\\\ (\\textdegree{}C)} \& \\multicolumn{1}{p{2.5cm}}{\\centering MAP \\\\ (mm y\\textsuperscript{-1})} \& \\multicolumn{1}{p{2.5cm}}{\\centering Temp. range \\\\ (\\textdegree{}C)} \& \\multicolumn{1}{p{2.5cm}}{\\centering CWD \\\\ (mm y\\textsuperscript{-1})} \\\\/' "inc/clim.tex"
