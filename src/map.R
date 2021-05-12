39aabbc6af991c83b02790b43b8907b1fbe214eb
John L. Godlee <johngodlee@gmail.com>
2021-05-12 10:27:02 +0100

    Improved site map

--- src/map.R
+++ src/map.R
@@ -18,8 +18,7 @@ source("functions.R")
 bicuar <- st_read("../dat/site_shp/bicuar/WDPA_Mar2018_protected_area_350-shapefile-polygons.shp")
 mtarure <- st_read("../dat/site_shp/mtarure/mtarure.shp")
 africa <- st_read("../dat/site_shp/africa/africa.shp")
-glob <- raster("/Volumes/john/Globcover2009_V2.3_Global_/GLOBCOVER_L4_200901_200912_V2.3.tif")
-glob_leg <- read_xls("/Volumes/john/Globcover2009_V2.3_Global_/Globcover2009_Legend.xls")
+white <- st_read("/Volumes/john/whiteveg/whiteveg_poly_joined.shp")
 plots <- read.csv("../dat/plot_corners.csv")
 
 # Join shapes
@@ -28,7 +27,7 @@ pa <- list(bicuar, mtarure)
 # Southern African countries
 saf <- africa %>%
   filter(iso3 %in% c("ZAF", "COD", "NAM", "ZMB", "BWA", "ZWE", "MOZ", "MWI", 
-      "AGO", "TZA", "COG", "RWA", "BDI", "UGA", "KEN", "SWZ"))
+      "AGO", "TZA", "COG", "RWA", "BDI", "UGA", "KEN", "SWZ", "LSO"))
 
 # Centroids of sites
 pa_cnt <- do.call(rbind, lapply(pa, function(x) {
@@ -37,19 +36,21 @@ pa_cnt <- do.call(rbind, lapply(pa, function(x) {
 pa_cnt$site <- c("AGO", "TZA")
 pa_cnt$name <- c("Bicuar", "Mtarure")
 
-# Contintental site map
-pdf(file = "../img/site_map.pdf", width = 4, height = 5.5)
-ggplot() + 
+site_map <- ggplot() + 
   geom_sf(data = saf, colour = "black", fill = "lightgrey") +
   geom_point(data = pa_cnt, aes(x = X, y = Y, fill = name), 
-    size = 2, shape = 21, colour = "black") + 
+    size = 3, shape = 21, colour = "black") + 
   geom_label_repel(data = pa_cnt, 
-    aes(x = X, y = Y, fill = name, label = name), colour = "black") + 
-  scale_fill_manual(values = pal[1:2]) + 
+    aes(x = X, y = Y, fill = name, label = name), colour = "black", show.legend = FALSE) + 
+  scale_fill_manual(name = "", values = pal[1:2]) + 
   ylim(-34, 5) +
   theme_classic() + 
   labs(x = "", y = "") + 
   theme(legend.position = "none")
+
+# Contintental site map
+pdf(file = "../img/site_map.pdf", width = 4, height = 5.5)
+site_map
 dev.off()
 
 # Get plot centres
@@ -79,17 +80,33 @@ plot_centre_fix$TZA <- plot_centre_split$TZA %>%
 
 plot_centre_wgs <- do.call(rbind, plot_centre_fix)
 
-glob_leg$col <- apply(glob_leg, 1, function(x) {
-  rgb(x[3], x[4], x[5], 255, max = 255)
-    })
+white_fil <- white %>% 
+  filter(leg_short_ %in% c(
+      "Dry deciduous forest & 2 grassland",
+      "Mopane",
+      "Wet miombo woodland",
+      "Dry miombo woodland",
+      "East African coastal mosaic")) %>% 
+  mutate(pal_col = rgb(leg_R, leg_G, leg_B)) %>%
+  mutate(leg_plot = case_when(
+    leg_short_ == "Dry deciduous forest & 2 grassland" ~ "Dry forest mosaic",
+    leg_short_ == "Mopane" ~ "Mopane",
+    leg_short_ == "Wet miombo woodland" ~ "Wet miombo",
+    leg_short_ == "Dry miombo woodland" ~ "Dry miombo", 
+    leg_short_ == "East African coastal mosaic" ~ "Coastal forest mosaic"))
+
 
 # Maps for each PA
 site_maps <- lapply(pa, function(x) {
 
   if (any(grepl("Bicuar", unlist(x)))) { 
     plot_centre_fix_fil <- plot_centre_fix$AGO
+    plot_pal <- pal[1]
+    plot_title <- "Bicuar"
   } else {
     plot_centre_fix_fil <- plot_centre_fix$TZA 
+    plot_pal <- pal[2]
+    plot_title <- "Mtarure"
   }
 
   plot_extent <- extent(plot_centre_fix_fil)
@@ -113,78 +130,36 @@ site_maps <- lapply(pa, function(x) {
     }
   }
 
-  glob_x <- crop(glob, extent(max_extent) + c(-0.02, 0.02, -0.02, 0.02))
-  glob_x_spdf <- as(glob_x, "SpatialPixelsDataFrame")
-  glob_x_df <- as.data.frame(glob_x_spdf)
-  colnames(glob_x_df) <- c("value", "x", "y")
-  glob_x_df$value <- as.character(glob_x_df$value)
+  white_x <- st_crop(st_make_valid(white_fil), 
+    extent(max_extent) + c(-0.02, 0.02, -0.02, 0.02))
 
-  glob_x_leg_fil <- glob_leg %>% 
-    filter(Value %in% glob_x_df$value) %>%
-    mutate(Value = as.character(Value)) %>%
-    dplyr::select(Value, Label, col)
+  white_x_pal <- white_x$pal_col
+  names(white_x_pal) <- white_x$leg_plot
 
-  glob_x_pal <- c(glob_x_leg_fil$col)
-  names(glob_x_pal) <- glob_x_leg_fil$Label
+  scale_x <- seq(plyr::round_any(max_extent[1], 0.2, round), plyr::round_any(max_extent[2], 0.2, round), 0.2)
+  scale_y <- seq(plyr::round_any(max_extent[3], 0.2, round), plyr::round_any(max_extent[4], 0.2, round), 0.2)
 
-  glob_x_df_clean <- left_join(glob_x_df, glob_x_leg_fil, 
-    by = c("value" = "Value"))
-
-
-  # GlobCover Bicuar map
   p <- ggplot() + 
-    geom_tile(data = glob_x_df_clean, aes(x = x, y = y, fill = Label)) +
+    geom_sf(data = white_x, aes(fill = leg_plot)) +
     geom_sf(data = x, colour = "black", size = 1, fill = NA) + 
     geom_point(data = plot_centre_fix_fil, aes(x = X, y = Y), 
-      shape = 21, size = 3, fill = "grey", colour = "black", 
+      shape = 21, size = 3, fill = plot_pal, colour = "black", 
       position = "jitter") + 
-    scale_fill_manual(name = "GlobCover", values = glob_x_pal) +
+    scale_fill_manual(name = "", values = white_x_pal) + 
     theme_classic() + 
-    theme(legend.position = "none") + 
-    labs(x = "", y = "")
+    theme(legend.position = "bottom") + 
+    labs(x = "", y = "") +
+    ggtitle(plot_title) + 
+    scale_x_continuous(breaks = scale_x) + 
+    scale_y_continuous(breaks = scale_y)
   
-  return(list(p, unique(glob_x_df_clean$value)))
+  return(p)
 })
 
 names(site_maps) <- c("Bicuar", "Mtarure")
 
-lapply(seq_along(site_maps), function(x) {
-  pdf(file = paste0("../img/", names(site_maps)[x], "_site_map.pdf"), 
-    width = 8, height = 8)
-  print(site_maps[[x]][[1]])
-  dev.off()
-})
-
-# Make plot with just the legend
-glob_leg_fil <- glob_leg %>% 
-  filter(Value %in% unique(unlist(lapply(site_maps, "[[", 2)))) %>%
-  mutate(label_new = case_when(
-    Value == "14" ~ "Rainfed cropland (14)",
-    Value == "20" ~ "Mosaic cropland (20)",
-    Value == "30" ~ "Mosaic grass/shrub/forest (30)",
-    Value == "40" ~ "Evergreen/deciduous broadleaf forest (40)",
-    Value == "50" ~ "Deciduous broadleaf forest (50)",
-    Value == "60" ~ "Deciduous broadleaf open woodland (60)",
-    Value == "90" ~ "Evergreen forest (90)",
-    Value == "110" ~ "Mosaic shrub/forest (110)",
-    Value == "120" ~ "Mosaic grassland (120)",
-    Value == "130" ~ "Shrubland (130)",
-    Value == "140" ~ "Grassland (140)",
-    Value == "160" ~ "Flooded broadleaf forest (160)",
-    Value == "200" ~ "Bare (200)",
-    Value == "210" ~ "Water (210)",
-    TRUE ~ NA_character_))
-
-pdf(file = "../img/site_map_legend.pdf", width = 5, height = 4)
-plot.new()
-par(xpd = NA)
-legend("center", 
-  legend = glob_leg_fil$label_new, 
-  pt.bg = glob_leg_fil$col, 
-  col = "black",
-  pch = 22, 
-  bty = "n", 
-  pt.cex = 2.5, 
-  cex = 1.2, 
-  text.col = "black")
+pdf(file = "../img/map.pdf", width = 14, height = 10)
+site_map + (site_maps[[1]] / site_maps[[2]]) + 
+  plot_layout(guides = 'collect') & 
+  theme(legend.position = "bottom")
 dev.off()
